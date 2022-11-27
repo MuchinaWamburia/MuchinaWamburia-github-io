4ace8c1282687c512b7099a4020f2f49f8268120
MuchinaWamburia <58048204+MuchinaWamburia@users.noreply.github.com>
2022-11-27 08:47:54 +0300

    Add files via upload

---
+++ scripts/style.js
@@ -0,0 +1,8 @@
+$(function () {
+  'use strict'
+
+  $('[data-toggle="style.css"]').on('click', function () {
+    $('.links').toggleClass('open')
+  })
+})
+// button onclick="document.location ='location'">btn-Name</a></button>
